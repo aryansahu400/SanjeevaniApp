9c8469487794d3c78b496c700760d7649f1c6aa9
Aryan Sahu <aryansahu400@gmail.com>
2023-01-20 21:29:03 +0530

    Day 8

--- src/in/scalive/gui/swingbuilder/RemoveFromComboBox.java
+++ src/in/scalive/gui/swingbuilder/RemoveFromComboBox.java
@@ -36,9 +36,13 @@ public class RemoveFromComboBox extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
+        jPanel1.setBackground(new java.awt.Color(235, 255, 241));
+
         cobSports.setFont(new java.awt.Font("Times New Roman", 0, 14)); // NOI18N
+        cobSports.setForeground(new java.awt.Color(51, 51, 51));
         cobSports.setMaximumRowCount(8);
         cobSports.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Football", "Cricket", "Hockey", "Kabbadi", "Volleyball", "Badminton", "Tennis", "Table tennis", "Fuseball", "Chess" }));
+        cobSports.setBorder(javax.swing.BorderFactory.createEtchedBorder(javax.swing.border.EtchedBorder.RAISED, java.awt.Color.black, java.awt.Color.lightGray));
         cobSports.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 cobSportsActionPerformed(evt);
@@ -63,28 +67,26 @@ public class RemoveFromComboBox extends javax.swing.JFrame {
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 388, Short.MAX_VALUE))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(64, 64, 64)
-                        .addComponent(cobSports, javax.swing.GroupLayout.PREFERRED_SIZE, 113, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(52, 52, 52)
-                        .addComponent(btnRemove)
-                        .addGap(0, 0, Short.MAX_VALUE)))
+                .addContainerGap()
+                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 388, Short.MAX_VALUE)
                 .addContainerGap())
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(56, 56, 56)
+                .addComponent(cobSports, javax.swing.GroupLayout.PREFERRED_SIZE, 113, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(btnRemove)
+                .addGap(53, 53, 53))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(15, 15, 15)
                 .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 46, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 57, Short.MAX_VALUE)
+                .addGap(40, 40, 40)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(btnRemove)
-                    .addComponent(cobSports, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(110, 110, 110))
+                    .addComponent(cobSports, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnRemove))
+                .addContainerGap(76, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -107,30 +109,33 @@ public class RemoveFromComboBox extends javax.swing.JFrame {
 
     private void btnRemoveActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRemoveActionPerformed
         String toRemove=null;
-        
-        while(toRemove==null || toRemove.equals("")){
-            toRemove=JOptionPane.showInputDialog(null,"Enter the name of sport you want to remove","Enter Input", JOptionPane.INFORMATION_MESSAGE);
-            toRemove=toRemove.trim();
-            if(toRemove==null || toRemove.equals(""))
-                JOptionPane.showMessageDialog(null, "Please enter a valid String");
-            else break;
-        }
-        toRemove=toRemove.toLowerCase();
-        int len=cobSports.getItemCount();
-        for(int i=0;i<len;i++){
-            String item=cobSports.getItemAt(i).toLowerCase();
-            if(item.equals(toRemove)){
-                int selection=JOptionPane.showConfirmDialog(null,"are you sure you want to remove "+item,"Confirmation Window",JOptionPane.OK_OPTION);
-                if(selection==JOptionPane.OK_OPTION){
-                    cobSports.removeItemAt(i);
-                    JOptionPane.showMessageDialog(null,item+" is removed","Removed",JOptionPane.INFORMATION_MESSAGE);
-                    return;
-                }else{
-                    return;
+        try{
+            while(toRemove==null || toRemove.equals("")){
+                toRemove=JOptionPane.showInputDialog(null,"Enter the name of sport you want to remove","Enter Input", JOptionPane.INFORMATION_MESSAGE);
+                toRemove=toRemove.trim();
+                if(toRemove==null || toRemove.equals(""))
+                    JOptionPane.showMessageDialog(null, "Please enter a valid String");
+                else break;
+            }
+            toRemove=toRemove.toLowerCase();
+            int len=cobSports.getItemCount();
+            for(int i=0;i<len;i++){
+                String item=cobSports.getItemAt(i).toLowerCase();
+                if(item.equals(toRemove)){
+                    int selection=JOptionPane.showConfirmDialog(null,"are you sure you want to remove "+item,"Confirmation Window",JOptionPane.OK_OPTION);
+                    if(selection==JOptionPane.OK_OPTION){
+                        cobSports.removeItemAt(i);
+                        JOptionPane.showMessageDialog(null,item+" is removed","Removed",JOptionPane.INFORMATION_MESSAGE);
+                        return;
+                    }else{
+                        return;
+                    }
                 }
             }
+            JOptionPane.showMessageDialog(null,"SPORT NOT FOUND","NOT FOUND",JOptionPane.WARNING_MESSAGE);
+        }catch(Exception e){
+            return;
         }
-        JOptionPane.showMessageDialog(null,"SPORT NOT FOUND","NOT FOUND",JOptionPane.WARNING_MESSAGE);
     }//GEN-LAST:event_btnRemoveActionPerformed
 
     /**
